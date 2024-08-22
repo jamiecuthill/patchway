53b1c4b2167ba367c57f5479d6c951068c34ad09
Jamie Cuthill <jamie.cuthill@justeattakeaway.com>
2024-08-22 15:57:49 +0100

    Add operation collection type that is easily converted to []byte

--- operation.go
+++ operation.go
@@ -1,6 +1,21 @@
 package patchway
 
+import "encoding/json"
+
 type Operation interface {
 	String() string
 	Bytes() []byte
+}
+
+// Operations are a collection of Operation
+// that can be applied to a resource.
+// It is used to represent a patch document.
+type Operations []Operation
+
+func (o Operations) Bytes() []byte {
+	b, err := json.Marshal(o)
+	if err != nil {
+		panic(err)
+	}
+	return b
 }
